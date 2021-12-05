5899e8a2fe008a966ea87a6173e404e9b67e6777
Kevin Nattinger <git@nattinger.net>
2021-12-04 17:35:47 -0800

    Add another overload for NSItemProvider

--- Sources/KNAsync/Delegate Replacements/NSItemProvider+Async.swift
+++ Sources/KNAsync/Delegate Replacements/NSItemProvider+Async.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension NSItemProvider {
     func loadObject<T: _ObjectiveCBridgeable>(ofClass cls: T.Type) async throws -> T where T._ObjectiveCType : NSItemProviderReading {
         try await withCheckedThrowingContinuation { continuation in
-            _ = loadObject(ofClass: cls) { (result: T?, error: Error?) in
+            _ = loadObject(ofClass: cls) { result, error in
                 guard let result = result else {
                     guard let error = error else {
                         continuation.resume(throwing: KNAsyncError.UnderlyingApiInvalidBehavior("NSItemProvider.loadObject returned neither result nor error"))
@@ -20,6 +20,26 @@ public extension NSItemProvider {
                     return
                 }
                 continuation.resume(returning: result)
+            }
+        }
+    }
+
+    func loadObject<T: NSItemProviderReading>(ofClass cls: T.Type) async throws -> T {
+        try await withCheckedThrowingContinuation { continuation in
+            _ = loadObject(ofClass: cls) { result, error in
+                guard let result = result else {
+                    guard let error = error else {
+                        continuation.resume(throwing: KNAsyncError.UnderlyingApiInvalidBehavior("NSItemProvider.loadObject returned neither result nor error"))
+                        return
+                    }
+                    continuation.resume(throwing: error)
+                    return
+                }
+                guard let typedResult = result as? T else {
+                    continuation.resume(throwing: KNAsyncError.UnderlyingApiInvalidBehavior("NSItemProvider.loadObject returned object of incorrect class: \(type(of: result)) is not compatible with \(T.self)"))
+                    return
+                }
+                continuation.resume(returning: typedResult)
             }
         }
     }
